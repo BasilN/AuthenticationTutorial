61c3fb2c1d878c52864f75d044c007b9537e800a
Basil Nyachogo <basil.nyachogo@gmail.com>
2015-01-21 13:22:10 -0500

    Linking up all auth methods

--- app/routes.js
+++ app/routes.js
@@ -77,8 +77,93 @@ module.exports= function(app, passport){
     failureRedirect : '/'
   }));
 
+  // =============================================================================
+  // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+  // =============================================================================
 
+  //locally ----------------------------------------------------------------------
+  app.get('/connect/local', function(req, res){
+    res.render('connect-local.ejs', {message: req.flash('loginMessage')});
+  });
+  app.post('/connect/local', passport.authenticate('local-signup', {
+    successRedirect : '/profile',
+    failureRedirect : '/connect/local',
+    failureFlash : true
+  }));
 
+  //facebook --------------------------------------------------------------------
+  app.get('/connect/facebook', passport.authorize('facebook', {
+    scope : 'email'
+  }));
+
+  app.get('/connect/facebook/callback', passport.authorize('facebook',{
+      successRedirect : '/profile',
+      failureRedirect : '/'
+  }));
+
+  //twitter --------------------------------------------------------------------
+  app.get('/connect/twitter', passport.authorize('twitter', {
+    scope : 'email'
+  }));
+
+  app.get('/connect/twitter/callback', passport.authorize('twitter', {
+    successRedirect :'/profile',
+    failureRedirect :'/'
+  }));
+
+  //google ---------------------------------------------------------------------
+  app.get('/connect/google', passport.authorize('google', {
+    scope :['profile', 'email']
+  }));
+
+  app.get('/connect/google/callback', passport.authorize('google', {
+    successRedirect : '/profile',
+    failureRedirect : '/'
+  }));
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  //local ----------------------------------------------------------
+  app.get('/unlink/local', function(req, res) {
+    var user            = req.user;
+    user.local.email    = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  //facebook --------------------------------------------------
+  app.get('/unlink/facebook', function(req, res){
+    var user            = req.user;
+    user.facebook.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  // twitter --------------------------------
+  app.get('/unlink/twitter', function(req, res) {
+    var user           = req.user;
+    user.twitter.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+  // google ---------------------------------
+  app.get('/unlink/google', function(req, res) {
+    var user          = req.user;
+    user.google.token = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
 
   // ============================================================
   // PROFILE SECTION ============================================
